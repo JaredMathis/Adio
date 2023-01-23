fb5f5e108ae3a17086b4a5294e45431a9c5a34db
JaredMathis <jared.mathis@native.consulting>
2023-01-23 18:37:11 -0500

    Mon Jan 23 2023 18:37:11 GMT-0500 (Eastern Standard Time)

--- public/index.mjs
+++ public/index.mjs
@@ -1,3 +1,5 @@
+let paused = false;
+
 if (annyang) {
     annyang.addCallback('resultNoMatch', result => {
         process_audio(result[0])
@@ -9,6 +11,7 @@ if (annyang) {
 let previous = [];
 
 function process_audio(input_string) {
+    console.log('processing ' + input_string);
     let non_empty = string_split_by_space(input_string);
     let filtered = list_string_non_empty(non_empty);
     previous.push(...filtered);
@@ -17,11 +20,19 @@ function process_audio(input_string) {
 }
 
 function audio_speak(words) {
+    console.log('paused')
+    annyang.abort();
+
     var msg = new SpeechSynthesisUtterance();
     msg.text = words.join(' ');
     window.speechSynthesis.speak(msg);
-
     console.log(msg.text)
+
+    msg.onend = (event) => {
+        console.log('finished ' + words);
+        annyang.start();
+    }
+
 }
 
 function list_string_non_empty(s) {
