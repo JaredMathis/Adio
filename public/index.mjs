8d86a35f7010223023b593b886cf7f9af0a455ce
JaredMathis <jared.mathis@native.consulting>
2023-01-23 17:59:51 -0500

    Mon Jan 23 2023 17:59:51 GMT-0500 (Eastern Standard Time)

---
+++ public/index.mjs
@@ -0,0 +1,12 @@
+if (annyang) {
+    // Let's define a command.
+    const commands = {
+      'hello': () => { alert('Hello world!'); }
+    };
+  
+    // Add our commands to annyang
+    annyang.addCommands(commands);
+  
+    // Start listening.
+    annyang.start();resultNoMatch 
+}
