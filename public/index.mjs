6b201110e5a7fdea76e808aa5f722c40b8299c8a
JaredMathis <jared.mathis@native.consulting>
2023-01-23 18:04:55 -0500

    Mon Jan 23 2023 18:04:55 GMT-0500 (Eastern Standard Time)

--- public/index.mjs
+++ public/index.mjs
@@ -1,12 +1,21 @@
 if (annyang) {
-    // Let's define a command.
-    const commands = {
-      'hello': () => { alert('Hello world!'); }
-    };
+    annyang.addCallback('resultNoMatch', result => {
+        process(result[0])
+    })
   
-    // Add our commands to annyang
-    annyang.addCommands(commands);
-  
-    // Start listening.
-    annyang.start();resultNoMatch 
+    annyang.start(); 
+}
+
+function process(input_string) {
+    let non_empty = string_split_by_space(input_string);
+    let filtered = list_string_non_empty(non_empty);
+    alert(filtered);
+}
+
+function list_string_non_empty(s) {
+    return s.filter(i => i.length > 0)
+}
+
+function string_split_by_space(s) {
+    return s.split(' ')
 }
